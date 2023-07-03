457b6080becff941a06747ba781397b0508d7663
Vinícius Soares <viniciussou222@gmail.com>
2023-07-03 16:43:53 -0300

    Projeto criado

---
+++ src/main/java/ViniciusSoares96/com/github/PraticaModelagemDeDados1Application.java
@@ -0,0 +1,13 @@
+package ViniciusSoares96.com.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticaModelagemDeDados1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticaModelagemDeDados1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ViniciusSoares96/com/github/PraticaModelagemDeDados1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ViniciusSoares96.com.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticaModelagemDeDados1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

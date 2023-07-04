f04fc417d57823b456bb45776fb1857af739eb47
Vinícius Soares <viniciussou222@gmail.com>
2023-07-04 13:17:45 -0300

    Criando operacao de instanciacao

--- src/main/java/ViniciusSoares96/com/github/PraticaModelagemDeDados1Application.java
+++ src/main/java/ViniciusSoares96/com/github/PraticaModelagemDeDados1Application.java
@@ -1,13 +1,34 @@
 package ViniciusSoares96.com.github;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ViniciusSoares96.com.github.domain.Categoria;
+import ViniciusSoares96.com.github.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class PraticaModelagemDeDados1Application {
+public class PraticaModelagemDeDados1Application implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PraticaModelagemDeDados1Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+	}
+
+	
+	
 }
